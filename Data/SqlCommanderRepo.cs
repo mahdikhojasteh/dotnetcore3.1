f4bb2f31fd960c56198e16511122e6bcc7b721ad
Mahdi Khojasteh <khojasteh.mahdi94@gmail.com>
2020-12-22 21:32:22 +0330

    initialized project

---
+++ Data/SqlCommanderRepo.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly commandifyDBContext _context;
+
+        public SqlCommanderRepo(commandifyDBContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Command.Add(cmd);
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+            _context.Command.Remove(cmd);
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Command.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Command.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            //Nothing
+        }
+    }
+}
---
+++ Models/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Commander.Models
+{
+    public partial class Command
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("command")]
+        [StringLength(100)]
+        public string Command1 { get; set; }
+        [Required]
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Required]
+        [Column("platform")]
+        [StringLength(100)]
+        public string Platform { get; set; }
+    }
+}
